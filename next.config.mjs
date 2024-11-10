21817e4fe58c2dd99c9b88439f233af0b77009aa
Mussarat shamsher <musaratskhan@github.com>
2024-11-11 01:39:36 +0500

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
 
 
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-/* ...Your other config rules */
-}
+// const nextConfig = {
+//     eslint: {
+//         ignoreDuringBuilds: true,
+//     },
+// /* ...Your other config rules */
+// }
 
 module.exports = nextConfig
+module.exports = {
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+  }
